a367482faa88136c9ca0ea160de3f286f09e686b
OpenROV User <rov@OpenROV.(none)>
2014-08-08 18:09:42 +0000

    working with navdata and status, no headers

--- index.js
+++ index.js
@@ -1,43 +1,24 @@
 function ros(name, deps) {
-    console.log("This is where plugin code for ros loads in the node process.");
-
-
-  //var vm = require('vm');
-
-  function include(path) {
-        var vm = require('vm');
-        var fs = require('fs');
-        var code = fs.readFileSync(path, 'utf-8');
-        //vm.runInThisContext(code, path);
-        eval(code+'');
-  }
-
-  include("/opt/openrov/cockpit/src/plugins/ros/roslibjs/src/util/shim/EventEmitter_fromweb.js");
-  include("/opt/openrov/cockpit/src/plugins/ros/roslibjs/build/roslib.min.js");
- 
+  console.log("ROS plugin started");
 
   // Start a ROS session 
+  var ROSLIB = require("roslib")
   var ros = new ROSLIB.Ros({
         url : 'ws://192.168.254.2:9090'
   });
 
   ros.on('connection', function() {
-     var fbDiv = document.getElementById('feedback');
-     fbDiv.innerHTML += "<p>Connected to websocket server.</p>";
   });
 
   ros.on('error', function(error) {
-    var fbDiv = document.getElementById('feedback');
-    fbDiv.innerHTML += "<p>Error connecting to websocket server.</p>";
   });
 
   ros.on('close', function() {
-    var fbDiv = document.getElementById('feedback');
-    fbDiv.innerHTML += "<p>Connection to websocket server closed.</p>";
   });
 
 
-  // These lines create a ROS topic and message 
+
+  // Create ROS topic and msg
   var cmdVelTopic = new ROSLIB.Topic({
     ros : ros,
     name : '/turtle1/cmd_vel',
@@ -57,31 +38,67 @@ function ros(name, deps) {
     }
   });
 
-  // To test if message gets published
-  // cmdVelTopic.publish(twist);
+  var rosDebug= new ROSLIB.Topic({
+     ros : ros,
+     name : '/openrov/debug',
+     messageType : 'std_msgs/String'
+  });
+
+  var debug = new ROSLIB.Message({
+     data : ''
+  });
+
+  var rosStatus = new ROSLIB.Topic({
+     ros : ros,
+     name : '/openrov/status',
+     messageType : 'openrov/rovstatus'
+  });
+
+  var status = new ROSLIB.Message({
+     data : ''
+  });
+
+  var rosNavData = new ROSLIB.Topic({
+     ros : ros,
+     name : '/openrov/navdata',
+     messageType : 'openrov/rovstatus'
+  });
+
+  var navdata = new ROSLIB.Message({
+     data : ''
+  });
+
 
   // This is how you would register a listner to traffic from the browser
-  // Listening to a Depth Hold Toggle from Cockpit and associating
-  // a publish ROS message command with it (I think)
+  // Listening to a Depth Hold Toggle from Cockpit and publishing a ROS message
 
-  /*
   deps.io.sockets.on('connection', function (socket) {
     socket.on('holdDepth_toggle', function () {
-      //sending on to the rov via the serial connection
-      cmdVelTopic.publish(twist);
-      deps.rov.send('msg(0)');
-      console.log('msg(0) sent');
+      debug.data = 'Hold depth toggled'
+      rosDebug.publish(debug);
+
+      //deps.rov.send('msg(0)');
+      //console.log('msg(0) sent');
     });
   });
-  */
+  
 
   //This is how you would register a listner to traffic from the ardunio
   //or other parts of the node modules and forward it to the browser
-  /*
-  deps.globalEventLoop.on('messageIwantToForward', function (data) {
-    deps.io.sockets.emit('messageIwantToForward', data);
+  
+  deps.rov.on('status', function (data) {
+    status.data = JSON.stringify(data);
+    rosStatus.publish(status);
+
+    //console.log(data);
+    //deps.io.sockets.emit('messageIwantToForward', data);
   });
-  */
+
+  deps.rov.on('navdata', function (data) {
+    navdata.data = JSON.stringify(data);
+    rosNavData.publish(navdata);
+  });
+
 };
 
 module.exports = ros;
